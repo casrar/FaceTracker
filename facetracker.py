1c03cd98a2e8f48298ca8eef18adec0dc1924814
Casrar <casey.j.morar@gmail.com>
2022-07-08 15:39:36 -0400

    added eye tracking

--- facetracker.py
+++ facetracker.py
@@ -1,12 +1,21 @@
 import cv2 
 
-haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 def identify(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face_rect = haar_cascade.detectMultiScale(gray, 
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = frame
+    face_rect = face_cascade.detectMultiScale(gray, 
     scaleFactor=1.1, minNeighbors=9)
-    for (x,y, w, h) in face_rect:
+    eye_rect = eye_cascade.detectMultiScale(gray, 
+    scaleFactor=1.1, minNeighbors=15)
+    for (x,y,w,h) in face_rect:
         cv2.rectangle(gray, (x,y), (x+w, y+h), (0,255,0), thickness=2)
+        cv2.putText(gray, 'Face', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        for (x,y,w,h) in eye_rect:
+            cv2.rectangle(gray, (x,y), (x+w, y+h), (0,0,255), thickness=2)
+            cv2.putText(gray, 'Eye', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
 
     cv2.imshow('frame', gray)
 
@@ -27,4 +36,3 @@ while True:
         break
 
 capture.release()
-#cv2.destroyAllWindow
