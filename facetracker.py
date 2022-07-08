f37dbdc30df89a0f357309b2eb0bd93aa6e406e3
Casrar <casey.j.morar@gmail.com>
2022-07-08 15:24:22 -0400

    added basic level face tracking

--- facetracker.py
+++ facetracker.py
@@ -1,5 +1,15 @@
 import cv2 
 
+haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+def identify(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_rect = haar_cascade.detectMultiScale(gray, 
+    scaleFactor=1.1, minNeighbors=9)
+    for (x,y, w, h) in face_rect:
+        cv2.rectangle(gray, (x,y), (x+w, y+h), (0,255,0), thickness=2)
+
+    cv2.imshow('frame', gray)
+
 capture = cv2.VideoCapture(0)
 if not capture.isOpened():
     print("Error with camera")
@@ -11,10 +21,10 @@ while True:
         print("Stream has ended.")
         break
     
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', gray)
+    identify(frame)
+    
     if cv2.waitKey(1) == ord('q'):
         break
 
 capture.release()
-cv2.destroyAllWindows()
+#cv2.destroyAllWindow
